5faf9c07a2ac9ecf79cc16e528d79041d9c96c30
Alena Chernova <alenvlasovav@gmail.com>
2025-03-30 10:01:41 +0300

    Добавляет корректный файл для оживления табов price

--- source/js/price/tabs.js
+++ source/js/price/tabs.js
@@ -1,15 +1,19 @@
+import { prices } from './model';
+import { renderPrice } from './view';
+
 const tabButtonsElements = document.querySelectorAll('.price__tab-button');
-const pricesElements = document.querySelectorAll('.price__rate-price');
 
-const renderPrice = (prices, currentIndex) => {
-  tabButtonsElements.forEach((element, elementIndex) => {
-    element.classList.toggle('price__tab-button--active', elementIndex === currentIndex);
-  });
+let currentIndex = 0;
 
-  pricesElements.forEach((element, index) => {
-    element.querySelector('span').textContent = prices[index];
-    element.dataset.price = prices[index];
+const initPrices = () => {
+  renderPrice(prices[currentIndex], currentIndex);
+
+  tabButtonsElements.forEach((buttonElement, index) => {
+    buttonElement.addEventListener('click', () => {
+      currentIndex = index;
+      renderPrice(prices[currentIndex], currentIndex);
+    });
   });
 };
 
-export { renderPrice };
+export { initPrices };
